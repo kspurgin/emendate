39a22b3b2b46bef22afe0e2f8ccea2097c7060b3
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2021-01-27 20:19:43 -0500

    added ordinal translation to workflow steps

--- lib/emendate/processing_manager.rb
+++ lib/emendate/processing_manager.rb
@@ -4,7 +4,7 @@ module Emendate
   class ProcessingManager
     include AASM
     attr_reader :orig_string, :norm_string, :tokens,
-      :orig_tokens, :converted_months, :errors
+      :orig_tokens, :converted_months, :translated_ordinals, :errors
     def initialize(string)
       @orig_string = string
       @norm_string = Emendate.normalize_orig(orig_string)
@@ -24,7 +24,7 @@ module Emendate
         transitions from: :tokenized, to: :months_converted, after: :perform_convert_months
       end
       event :translate_ordinals do
-        transitions from: :months_converted, to: :ordinals_translated
+        transitions from: :months_converted, to: :ordinals_translated, after: :perform_translate_ordinals
       end
       event :standardize_formats do
         transitions from: :ordinals_translated, to: :formats_standardized
@@ -34,14 +34,23 @@ module Emendate
         transitions from: :tokenized, to: :failed, guard: :errors?
         transitions from: :months_converted, to: :done, guard: :no_errors?
         transitions from: :months_converted, to: :failed, guard: :errors?
+        transitions from: :ordinals_translated, to: :done, guard: :no_errors?
+        transitions from: :ordinals_translated, to: :failed, guard: :errors?
       end
     end
 
     def process
       lex
       convert_months if may_convert_months?
+      translate_ordinals if may_translate_ordinals?
       finalize
     end
+
+    def state
+      aasm.current_state
+    end
+
+    private
 
     def perform_lex
       l = Emendate::Lexer.new(norm_string)
@@ -63,12 +72,18 @@ module Emendate
       @converted_months = tokens.dup
     end
 
-    def state
-      aasm.current_state
+    def perform_translate_ordinals
+      t = Emendate::OrdinalTranslator.new(tokens: converted_months)
+      begin
+        t.translate
+      rescue StandardError => e
+        errors << e
+      else
+        @tokens = t.result
+        @translated_ordinals = tokens.dup
+      end
     end
-
-    private
-
+    
     def log_status_change
       puts "changing from #{aasm.from_state} to #{aasm.to_state} (event: #{aasm.current_event})"
     end
