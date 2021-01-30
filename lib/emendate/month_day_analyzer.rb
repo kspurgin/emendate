b6a6fea69985d2e372d42084a7b6000d867529b9
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2021-01-29 20:17:14 -0500

    Create MonthDayAnalyzer to encapsulate the logic of handling numeric month and day values

---
+++ lib/emendate/month_day_analyzer.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'emendate/date_utils'
+
+module Emendate
+  class MonthDayAnalyzer
+    class MonthDayError < StandardError
+      def initialize(n1, n2, y)
+        m = "Cannot determine valid month/date assignment for #{n1.lexeme}-#{n2.lexeme}-#{y.lexeme}"
+        super(m)
+      end
+    end
+    
+    include DateUtils
+    attr_reader :month, :day
+
+    def initialize(n1, n2, y, opt)
+      @n1 = n1
+      @n2 = n2
+      @y = y
+      @opt = opt
+      analyze
+    end
+
+    private
+
+    def analyze
+      if !@n1.monthable? && @n2.monthable?
+        month_day = [@n2, @n1]
+      elsif @n1.monthable? && !@n2.monthable?
+        month_day = [@n1, @n2]
+      elsif @opt == :as_month_day
+        month_day = [@n1, @n2]
+      elsif @opt == :as_day_month
+        month_day = [@n2, @n1]
+      end
+
+      if valid_date?(@y, month_day[0], month_day[1])
+        @month = month_day[0]
+        @day = month_day[1]
+      else
+        raise MonthDayError.new(@n1, @n2, @y)
+      end
+    end
+  end
+end
---
+++ spec/lib/emendate/month_day_analyzer_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+RSpec.describe Emendate::MonthDayAnalyzer do
+  def prep(str, opt = :as_month_day)
+    pm = Emendate.prep_for(str, :tag_date_parts)
+    t = pm.standardized_formats
+    mda = Emendate::MonthDayAnalyzer.new(t[0], t[2], t[4], opt)
+    mda.month.nil? ? 'ambiguous' : "#{mda.month.lexeme} #{mda.day.lexeme}"
+  end
+  
+  context 'unambiguous month day - 12-31-2020' do
+    it 'returns expected' do
+      expect(prep('12-31-2001')).to eq('12 31')
+    end
+  end
+
+  context 'unambiguous day month - 31-12-2020' do
+    it 'returns expected' do
+      expect(prep('31-12-2001')).to eq('12 31')
+    end
+  end
+
+  context 'ambiguous - 02-03-2020' do
+    context 'default (month day)' do
+      it 'Feb 3' do
+        expect(prep('02-03-2020')).to eq('02 03')
+      end
+    end
+
+    context 'day month' do
+      it 'Feb 3' do
+        expect(prep('02-03-2020', :as_day_month)).to eq('03 02')
+      end
+    end
+
+    context 'invalid - 31-29-2020' do
+      it 'raises error' do
+        expect{ prep('31-29-2001') }.to raise_error(Emendate::MonthDayAnalyzer::MonthDayError)
+      end
+    end
+  end
+end
