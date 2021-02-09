3322b8f33851f4bbe00853340a473199b7c86b49
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2021-02-09 16:21:31 -0500

    more fuller testing of decade functionality

--- spec/lib/emendate/date_types/decade_spec.rb
+++ spec/lib/emendate/date_types/decade_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Emendate::DateTypes::Decade do
   
   context 'plural decade (1990s)' do
     before(:all) do
-      @dt = Emendate::DateTypes::Decade.new(decade: 1999, decade_type: :plural)
+      @dt = Emendate::DateTypes::Decade.new(decade: 1990, decade_type: :plural)
     end
     
     describe '#earliest' do
@@ -36,6 +36,78 @@ RSpec.describe Emendate::DateTypes::Decade do
       end
     end
   end
+
+  context 'plural decade (1900s)' do
+    before(:all) do
+      @dt = Emendate::DateTypes::Decade.new(decade: 1900, decade_type: :plural)
+    end
+    
+    describe '#earliest' do
+      it 'returns 1900-01-01' do
+        expect(@dt.earliest).to eq(Date.new(1900, 1, 1))
+      end
+    end
+
+    describe '#latest' do
+      it 'returns 1909-12-31' do
+        expect(@dt.latest).to eq(Date.new(1909, 12, 31))
+      end
+    end
+
+    describe '#lexeme' do
+      it 'returns 1900s' do
+        expect(@dt.lexeme).to eq('1900s')
+      end
+    end
+  end
+
+    context 'plural decade (2000s)' do
+    before(:all) do
+      @dt = Emendate::DateTypes::Decade.new(decade: 2000, decade_type: :plural)
+    end
+    
+    describe '#earliest' do
+      it 'returns 2000-01-01' do
+        expect(@dt.earliest).to eq(Date.new(2000, 1, 1))
+      end
+    end
+
+    describe '#latest' do
+      it 'returns 2009-12-31' do
+        expect(@dt.latest).to eq(Date.new(2009, 12, 31))
+      end
+    end
+
+    describe '#lexeme' do
+      it 'returns 2000s' do
+        expect(@dt.lexeme).to eq('2000s')
+      end
+    end
+    end
+
+    context 'plural decade (200s)' do
+      before(:all) do
+        @dt = Emendate::DateTypes::Decade.new(decade: 200, decade_type: :plural)
+      end
+      
+      describe '#earliest' do
+        it 'returns 0200-01-01' do
+          expect(@dt.earliest).to eq(Date.new(200, 1, 1))
+        end
+      end
+
+      describe '#latest' do
+        it 'returns 0209-12-31' do
+          expect(@dt.latest).to eq(Date.new(209, 12, 31))
+        end
+      end
+
+      describe '#lexeme' do
+        it 'returns 200s' do
+          expect(@dt.lexeme).to eq('200s')
+        end
+      end
+    end
 
   context 'uncertainty_digit decade (199u)' do
     before(:all) do
