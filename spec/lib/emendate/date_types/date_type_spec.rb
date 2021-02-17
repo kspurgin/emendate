ee5fd64e1d7c9bc25dfa03a685e0d6cbeeccb5eb
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2021-02-17 16:03:20 -0500

    add tests

---
+++ spec/lib/emendate/date_types/date_type_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+RSpec.describe Emendate::DateTypes::DateType do
+  before(:all) do
+    @dt = Emendate::DateTypes::DateType.new(literal: '2021')
+  end
+
+  it 'type = :datetype_date_type' do
+    expect(@dt.type).to eq(:datetype_date_type)
+  end
+
+  describe '#date_part?' do
+    it 'returns true' do
+      expect(@dt.date_part?).to be true
+    end
+  end
+  
+  describe '#earliest' do
+    it 'raise error' do
+      expect{ @dt.earliest }.to raise_error(NotImplementedError)
+    end
+  end
+
+  describe '#latest' do
+    it 'raise error' do
+      expect{ @dt.latest }.to raise_error(NotImplementedError)
+    end
+  end
+
+  describe '#lexeme' do
+    it 'raise error' do
+      expect{ @dt.lexeme }.to raise_error(NotImplementedError)
+    end
+  end
+  
+  describe '#range?' do
+    it 'raise error' do
+      expect{ @dt.range? }.to raise_error(NotImplementedError)
+    end
+  end
+end
