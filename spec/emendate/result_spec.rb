f077d5545aece17088a2509c66b63f02f5e8d167
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-05-26 15:39:58 -0400

    fix tests with irrelevant 2-digit year rollover issues

--- spec/emendate/result_spec.rb
+++ spec/emendate/result_spec.rb
@@ -14,9 +14,9 @@ RSpec.describe Emendate::Result do
 
   describe '#to_h' do
     it 'returns hash' do
-      res = Emendate.parse('mid 1800s to 2/23/21').to_h
-      expected = { original_string: 'mid 1800s to 2/23/21',
-                   dates: [{ original_string: 'mid 1800s to 2/23/21',
+      res = Emendate.parse('mid 1800s to 2/23/1921').to_h
+      expected = { original_string: 'mid 1800s to 2/23/1921',
+                   dates: [{ original_string: 'mid 1800s to 2/23/1921',
                              index_dates: [],
                              date_start: nil,
                              date_end: nil,
@@ -32,9 +32,9 @@ RSpec.describe Emendate::Result do
 
   describe '#to_json' do
     it 'returns JSON escaped string' do
-      res = Emendate.parse('mid 1800s to 2/23/21').to_json
+      res = Emendate.parse('mid 1800s to 2/23/1921').to_json
       expected = <<~LONGSTRING
-      {\"original_string\":\"mid 1800s to 2/23/21\",\"dates\":[{\"original_string\":\"mid 1800s to 2/23/21\",\"index_dates\":[],\"date_start\":null,\"date_end\":null,\"date_start_full\":\"1804-01-01\",\"date_end_full\":\"1921-02-23\",\"inclusive_range\":true,\"certainty\":[]}],\"errors\":[],\"warnings\":[\"Interpreting pluralized year as decade\"]}
+      {\"original_string\":\"mid 1800s to 2/23/1921\",\"dates\":[{\"original_string\":\"mid 1800s to 2/23/1921\",\"index_dates\":[],\"date_start\":null,\"date_end\":null,\"date_start_full\":\"1804-01-01\",\"date_end_full\":\"1921-02-23\",\"inclusive_range\":true,\"certainty\":[]}],\"errors\":[],\"warnings\":[\"Interpreting pluralized year as decade\"]}
       LONGSTRING
       expect(res).to eq(expected.chomp)
     end
