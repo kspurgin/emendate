bf5be0cc090d60a2c42553479f13007816573d29
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-05-26 15:21:52 -0400

    globally aggregate_failures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,9 @@ require 'pry'
 RSpec.configure do |config|
   config.include Helpers
 
+  config.define_derived_metadata do |meta|
+    meta[:aggregate_failures] = true
+  end
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
 
