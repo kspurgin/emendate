c97247fee6de7dd23b44fb15659d52cdf512fe74
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-06-02 11:10:05 -0400

    rspec: random order, with deterministic (repeatable) seed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,6 +24,9 @@ RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
 
+  config.order = :random
+  Kernel.srand config.seed
+  
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
