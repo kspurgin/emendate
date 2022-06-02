a9880b37809768c672334a0a8deeaec591bcf3ca
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-06-02 11:07:14 -0400

    handle config when testing

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -5,9 +5,9 @@ require 'emendate'
 module Helpers
   module_function
 
-  Emendate.config.examples.file_name = 'spec_fixture.csv'
 
   def test_rows(str, opt)
+    Emendate.config.examples.file_name = 'spec_fixture.csv'
     Emendate::Examples::Csv.rows(str, opt)
       .sort_by{ |row| row.dateval_occurrence }
   end
