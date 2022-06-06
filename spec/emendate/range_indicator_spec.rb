fdb8327fd79979039ddc669c330fee9424012c1b
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-06-06 17:47:16 -0400

    move warning about invalid ranges to RangeIndicator; handle mixed range/non-range strings

--- spec/emendate/range_indicator_spec.rb
+++ spec/emendate/range_indicator_spec.rb
@@ -3,27 +3,45 @@
 require 'spec_helper'
 
 RSpec.describe Emendate::RangeIndicator do
+  subject(:indicator){ described_class.new(tokens: tokens) }
 
-  def indicate(str, options = {})
-    pm = Emendate.prep_for(str, :indicate_ranges, options)
-    ri = Emendate::RangeIndicator.new(tokens: pm.tokens)
-    ri.indicate
-  end
+  let(:tokens){ Emendate.prep_for(str, :indicate_ranges, options).tokens }
+  let(:options){ {} }
 
   describe '#indicate' do
+    let(:result) { indicator.indicate }
+    let(:type_string){ result.type_string }
+    
     context 'without range present (circa 202127)' do
-      before(:all){ @i = indicate('circa 202127') }
+      let(:str){ 'circa 202127' }
 
       it 'returns original' do
-        expect(@i.type_string).to eq('year_date_type')
+        expect(type_string).to eq('year_date_type')
       end
     end
 
     context 'with range present (1972 - 1999)' do
-      before(:all){ @i = indicate('1972 - 1999') }
+      let(:str){ '1972 - 1999' }
 
       it 'returns range_date_type' do
-        expect(@i.type_string).to eq('range_date_type')
+        expect(type_string).to eq('range_date_type')
+      end
+    end
+
+    context 'with mixed range and non-range (1970, 1972 - 1999, 2002)' do
+      let(:str){ '1970, 1972 - 1999, 2002' }
+
+      it 'returns range_date_type' do
+        expect(type_string).to eq('year_date_type comma range_date_type comma year_date_type')
+      end
+    end
+
+    context 'with invalid range present (1999 - 1972)' do
+      let(:str){ '1999 - 1972' }
+
+      it 'returns range_date_type' do
+        expect(type_string).to eq('range_date_type')
+        expect(indicator.warnings.length).to eq(1)
       end
     end
   end
