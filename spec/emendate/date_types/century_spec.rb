79eefefe283c579ea6dc4d5b854fe9339a6e1f4c
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-06-07 11:48:41 -0400

    refactor tests

--- spec/emendate/date_types/century_spec.rb
+++ spec/emendate/date_types/century_spec.rb
@@ -3,14 +3,19 @@
 require 'spec_helper'
 
 RSpec.describe Emendate::DateTypes::Century do
+  subject(:century){ described_class.new(**args) }
+  
   context 'when called without century_type' do
+    let(:args){ {literal: 19} }
+    
     it 'raises error' do
-      err = Emendate::DateTypes::MissingCenturyTypeError
-      expect{described_class.new(literal: 19) }.to raise_error(err)
+      expect{ subject }.to raise_error(Emendate::DateTypes::MissingCenturyTypeError)
     end
   end
 
   context 'when called with unsupported century_type value' do
+    let(:args) { {} }
+    
     it 'raises error' do
       err = Emendate::DateTypes::CenturyTypeValueError
       expect{described_class.new(literal: 19, century_type: :misc) }.to raise_error(err)
@@ -18,186 +23,124 @@ RSpec.describe Emendate::DateTypes::Century do
   end
 
   context 'with textual century name (19th)' do
-    before(:all) do
-      @dt = described_class.new(literal: 19, century_type: :name)
-    end
-
-    it 'type = :century_date_type' do
-      expect(@dt.type).to eq(:century_date_type)
-    end
-
-    describe '#earliest' do
-      context 'without partial_indicator' do
-        it 'returns 1801-01-01' do
-          expect(@dt.earliest).to eq(Date.new(1801, 1, 1))
-        end
-      end
-
-      context 'with early' do
-        it 'returns 1801-01-01' do
-          dt = described_class.new(literal: 19, century_type: :name, partial_indicator: 'early')
-          expect(dt.earliest).to eq(Date.new(1801, 1, 1))
-        end
-      end
-
-      context 'with mid' do
-        it 'returns 1834-01-01' do
-          dt = described_class.new(literal: 19, century_type: :name, partial_indicator: 'mid')
-          expect(dt.earliest).to eq(Date.new(1834, 1, 1))
-        end
-      end
-
-      context 'with late' do
-        it 'returns 1867-01-01' do
-          dt = described_class.new(literal: 19, century_type: :name, partial_indicator: 'late')
-          expect(dt.earliest).to eq(Date.new(1867, 1, 1))
-        end
-      end
-    end
-
-    describe '#latest' do
-      context 'without partial_indicator' do
-        it 'returns 1900-12-31' do
-          expect(@dt.latest).to eq(Date.new(1900, 12, 31))
-        end
-      end
-
-      context 'with early' do
-        it 'returns 1834-12-31' do
-          dt = described_class.new(literal: 19, century_type: :name, partial_indicator: 'early')
-          expect(dt.latest).to eq(Date.new(1834, 12, 31))
-        end
-      end
-
-      context 'with mid' do
-        it 'returns 1867-12-31' do
-          dt = described_class.new(literal: 19, century_type: :name, partial_indicator: 'mid')
-          expect(dt.latest).to eq(Date.new(1867, 12, 31))
-        end
-      end
-
-      context 'with late' do
-        it 'returns 1900-12-31' do
-          dt = described_class.new(literal: 19, century_type: :name, partial_indicator: 'late')
-          expect(dt.latest).to eq(Date.new(1900, 12, 31))
-        end
-      end
-    end
-
-    describe '#earliest_at_granularity' do
-      context 'without partial_indicator' do
-        it 'returns 1801' do
-          expect(@dt.earliest_at_granularity).to eq(1801)
-        end
-      end
-    end
-
-    describe '#lexeme' do
-      it 'returns 19 century' do
-        expect(@dt.lexeme).to eq('19 century')
-      end
+    let(:args) { {literal: 19, century_type: :name} }
+    
+    it 'returns expected values' do
+      expect(subject.type).to eq(:century_date_type)
+      expect(subject.earliest).to eq(Date.new(1801, 1, 1))
+      expect(subject.latest).to eq(Date.new(1900, 12, 31))
+      expect(subject.earliest_at_granularity).to eq(1801)
+      expect(subject.latest_at_granularity).to eq(1900)
+      expect(subject.lexeme).to eq('19 century')
+      expect(subject.literal).to eq(19)
     end
   end
 
-  context 'with plural century (1900s)' do
-    before(:all) do
-      @dt = described_class.new(literal: 19, century_type: :plural)
+  context 'with textual century name (19th) and partial indicator: early' do
+    let(:args) { {literal: 19, century_type: :name, partial_indicator: 'early'} }
+    
+    it 'returns expected values' do
+      expect(subject.type).to eq(:century_date_type)
+      expect(subject.earliest).to eq(Date.new(1801, 1, 1))
+      expect(subject.latest).to eq(Date.new(1834, 12, 31))
+      expect(subject.earliest_at_granularity).to eq(1801)
+      expect(subject.latest_at_granularity).to eq(1834)
+      expect(subject.lexeme).to eq('19 century')
+      expect(subject.literal).to eq(19)
     end
+  end
 
-    describe '#earliest' do
-      context 'without partial_indicator' do
-        it 'returns 1900-01-01' do
-          expect(@dt.earliest).to eq(Date.new(1900, 1, 1))
-        end
-      end
-
-      context 'with early' do
-        it 'returns 1900-01-01' do
-          dt = described_class.new(literal: 19, century_type: :plural, partial_indicator: 'early')
-          expect(dt.earliest).to eq(Date.new(1900, 1, 1))
-        end
-      end
-
-      context 'with mid' do
-        it 'returns 1933-01-01' do
-          dt = described_class.new(literal: 19, century_type: :plural, partial_indicator: 'mid')
-          expect(dt.earliest).to eq(Date.new(1933, 1, 1))
-        end
-      end
-
-      context 'with late' do
-        it 'returns 1966-01-01' do
-          dt = described_class.new(literal: 19, century_type: :plural, partial_indicator: 'late')
-          expect(dt.earliest).to eq(Date.new(1966, 1, 1))
-        end
-      end
+  context 'with textual century name (19th) and partial indicator: mid' do
+    let(:args) { {literal: 19, century_type: :name, partial_indicator: 'mid'} }
+    
+    it 'returns expected values' do
+      expect(subject.type).to eq(:century_date_type)
+      expect(subject.earliest).to eq(Date.new(1834, 1, 1))
+      expect(subject.latest).to eq(Date.new(1867, 12, 31))
+      expect(subject.earliest_at_granularity).to eq(1834)
+      expect(subject.latest_at_granularity).to eq(1867)
+      expect(subject.lexeme).to eq('19 century')
+      expect(subject.literal).to eq(19)
     end
+  end
 
-    describe '#latest' do
-      context 'without partial_indicator' do
-        it 'returns 1999-12-31' do
-          expect(@dt.latest).to eq(Date.new(1999, 12, 31))
-        end
-      end
-
-      context 'with early' do
-        it 'returns 1933-12-31' do
-          dt = described_class.new(literal: 19, century_type: :plural, partial_indicator: 'early')
-          expect(dt.latest).to eq(Date.new(1933, 12, 31))
-        end
-      end
-
-      context 'with mid' do
-        it 'returns 1966-12-31' do
-          dt = described_class.new(literal: 19, century_type: :plural, partial_indicator: 'mid')
-          expect(dt.latest).to eq(Date.new(1966, 12, 31))
-        end
-      end
-
-      context 'with late' do
-        it 'returns 1999-12-31' do
-          dt = described_class.new(literal: 19, century_type: :plural, partial_indicator: 'late')
-          expect(dt.latest).to eq(Date.new(1999, 12, 31))
-        end
-      end
+  context 'with textual century name (19th) and partial indicator: late' do
+    let(:args) { {literal: 19, century_type: :name, partial_indicator: 'late'} }
+    
+    it 'returns expected values' do
+      expect(subject.type).to eq(:century_date_type)
+      expect(subject.earliest).to eq(Date.new(1867, 1, 1))
+      expect(subject.latest).to eq(Date.new(1900, 12, 31))
+      expect(subject.earliest_at_granularity).to eq(1867)
+      expect(subject.latest_at_granularity).to eq(1900)
+      expect(subject.lexeme).to eq('19 century')
+      expect(subject.literal).to eq(19)
     end
+  end
 
-    describe '#latest_at_granularity' do
-      context 'without partial_indicator' do
-        it 'returns 1999' do
-          expect(@dt.latest_at_granularity).to eq(1999)
-        end
-      end
+  context 'with plural century name (1900s)' do
+    let(:args) { {literal: 19, century_type: :plural} }
+    
+    it 'returns expected values' do
+      expect(subject.type).to eq(:century_date_type)
+      expect(subject.earliest).to eq(Date.new(1900, 1, 1))
+      expect(subject.latest).to eq(Date.new(1999, 12, 31))
+      expect(subject.earliest_at_granularity).to eq(1900)
+      expect(subject.latest_at_granularity).to eq(1999)
+      expect(subject.lexeme).to eq('1900s')
+      expect(subject.literal).to eq(19)
     end
+  end
 
-    describe '#lexeme' do
-      it 'returns 1900s' do
-        expect(@dt.lexeme).to eq('1900s')
-      end
+  context 'with plural century name (1900s) and partial indicator: early' do
+    let(:args) { {literal: 19, century_type: :plural, partial_indicator: 'early'} }
+    
+    it 'returns expected values' do
+      expect(subject.type).to eq(:century_date_type)
+      expect(subject.earliest).to eq(Date.new(1900, 1, 1))
+      expect(subject.latest).to eq(Date.new(1933, 12, 31))
+      expect(subject.earliest_at_granularity).to eq(1900)
+      expect(subject.latest_at_granularity).to eq(1933)
+      expect(subject.lexeme).to eq('1900s')
+      expect(subject.literal).to eq(19)
+    end
+  end
+
+  context 'with plural century name (1900s) and partial indicator: mid' do
+    let(:args) { {literal: 19, century_type: :plural, partial_indicator: 'mid'} }
+    
+    it 'returns expected values' do
+      expect(subject.type).to eq(:century_date_type)
+      expect(subject.earliest).to eq(Date.new(1933, 1, 1))
+      expect(subject.latest).to eq(Date.new(1966, 12, 31))
+      expect(subject.earliest_at_granularity).to eq(1933)
+      expect(subject.latest_at_granularity).to eq(1966)
+      expect(subject.lexeme).to eq('1900s')
+      expect(subject.literal).to eq(19)
+    end
+  end
+
+  context 'with plural century name (1900s) and partial indicator: late' do
+    let(:args) { {literal: 19, century_type: :plural, partial_indicator: 'late'} }
+    
+    it 'returns expected values' do
+      expect(subject.type).to eq(:century_date_type)
+      expect(subject.earliest).to eq(Date.new(1966, 1, 1))
+      expect(subject.latest).to eq(Date.new(1999, 12, 31))
+      expect(subject.earliest_at_granularity).to eq(1966)
+      expect(subject.latest_at_granularity).to eq(1999)
+      expect(subject.lexeme).to eq('1900s')
+      expect(subject.literal).to eq(19)
     end
   end
 
   context 'with uncertainty_digit century (19uu)' do
-    before(:all) do
-      @dt = described_class.new(literal: 19, century_type: :uncertainty_digits)
-    end
-
-    describe '#earliest' do
-      it 'returns 1900-01-01' do
-        expect(@dt.earliest).to eq(Date.new(1900, 1, 1))
-      end
-    end
-
-    describe '#latest' do
-      it 'returns 1999-12-31' do
-        expect(@dt.latest).to eq(Date.new(1999, 12, 31))
-      end
-    end
-
-    describe '#lexeme' do
-      it 'returns 19uu' do
-        expect(@dt.lexeme).to eq('19uu')
-      end
+    let(:args) { {literal: 19, century_type: :uncertainty_digits} }
+    
+    it 'returns expected values' do
+      expect(century.earliest).to eq(Date.new(1900, 1, 1))
+      expect(century.latest).to eq(Date.new(1999, 12, 31))
+      expect(century.lexeme).to eq('19uu')
     end
   end
 end
