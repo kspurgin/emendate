18de98164ce7d75cdac8fea9524d8448a70bae69
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-12-09 16:00:44 -0500

    Add new normalize method that returns monad

--- lib/emendate.rb
+++ lib/emendate.rb
@@ -9,6 +9,8 @@ require 'aasm'
 require 'active_support'
 require 'active_support/core_ext/object'
 require 'dry-configurable'
+require 'dry/monads'
+require 'dry/monads/do'
 
 require 'emendate/error'
 require 'emendate/date_types/date_type'
@@ -24,6 +26,7 @@ module Emendate
   include ExampleHelpers
   extend self
   extend Dry::Configurable
+  extend Dry::Monads[:result]
 
   LQ = "\u201C"
   RQ = "\u201D"
@@ -81,6 +84,27 @@ module Emendate
       .sub(/^ *date unknown?\.? *$/, 'dateunknown')
       .sub(/^ *unknown date?\.? *$/, 'unknowndate')
       .sub(/(st|nd|rd|th) c\.?$/, '\1 century') # ending c after ordinal
+  end
+
+  # @param orig [String]
+  # @return [String]
+  def normalize(orig)
+    result = orig.downcase.sub('[?]', '?')
+      .sub('(?)', '?')
+      .sub(/^c([^a-z])/, 'circa\1') # initial c followed by non-letter
+      .gsub(/b\.?c\.?(e\.?|)/, 'bce') # cleanup bc, bce
+      .gsub(/(a\.?d\.?|c\.?e\.?)/, 'ce') # cleanup ad, ce
+      .gsub(/b\.?p\.?/, 'bp') # cleanup bp
+      .sub(/^n\.? ?d\.?$/, 'nodate') # cleanup nd
+      .sub(/^ *not dated *$/, 'notdated') # cleanup not dated
+      .sub(/^ *unkn?\.? *$/, 'unk') # cleanup unk.
+      .sub(/^ *date unknown?\.? *$/, 'dateunknown')
+      .sub(/^ *unknown date?\.? *$/, 'unknowndate')
+      .sub(/(st|nd|rd|th) c\.?$/, '\1 century') # ending c after ordinal
+  rescue StandardError => err
+    Failure(err)
+  else
+    Success(result)
   end
 
   # str = String to process
--- spec/emendate_spec.rb
+++ spec/emendate_spec.rb
@@ -4,4 +4,22 @@ RSpec.describe Emendate do
   it 'has a version number' do
     expect(Emendate::VERSION).not_to be nil
   end
+
+  describe '#normalize' do
+    let(:result){ Emendate.normalize('foo') }
+    it 'returns Success' do
+      expect(result).to be_a(Dry::Monads::Success)
+    end
+
+    it 'normalizes as expected' do
+      examples = {
+        'c. 1882(?)'=>'circa. 1882?',
+        '16th c.'=> '16th century',
+        '2 B.C.'=>'2 bce'
+      }
+      results = examples.keys
+        .map{ |str| Emendate.normalize(str).value! }
+      expect(results).to eq(examples.values)
+    end
+  end
 end
