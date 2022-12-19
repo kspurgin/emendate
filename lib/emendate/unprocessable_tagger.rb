07cd84ca61471e795873738793d5a5be0f15aece
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-12-18 19:54:59 -0500

    Fix UnprocessableTagger

--- lib/emendate/unprocessable_tagger.rb
+++ lib/emendate/unprocessable_tagger.rb
@@ -2,41 +2,46 @@
 
 module Emendate
   class UnprocessableTagger
-    attr_reader :str, :result
+    include Dry::Monads[:result]
 
-    Patterns = [
-      /^Y-\d+$/,
-      /XXXX-\d{2}-XX/,
-      /\dXXX-XX/,
-      /\dXXX-\d{2}/,
-      /\d{2}XX-\d{2}-\d{2}/,
-      /\d{3}X-\d{2}-\d{2}/,
-      /^\d{3,4}S\d+$/,
-      /\d{4}-\d{2}-XX/
-    ]
-    Re = Regexp.union(Patterns)
-    
-    def initialize(tokens:, str:)
-      @tokens = tokens
-      @str = str
-      @result = Emendate::SegmentSets::MixedSet.new
+    class << self
+      def call(...)
+        self.new(...).call
+      end
     end
 
-    def tag
-      unless str.match?(Re)
-        passthrough
-        return
-      end
+    Patterns = [
+      /^y-\d+$/,
+      /xxxx-\d{2}-xx/,
+      /\dxxx-xx/,
+      /\dxxx-\d{2}/,
+      /\d{2}xx-\d{2}-\d{2}/,
+      /\d{3}x-\d{2}-\d{2}/,
+      /^\d{3,4}S\d+$/,
+      /\d{4}-\d{2}-xx/
+    ]
+    Re = Regexp.union(Patterns)
 
-      result << Emendate::DateTypes::Unprocessable.new(children: @tokens.segments, literal: str)
+    def initialize(tokens)
+      @tokens = tokens
+      @str = tokens.segments
+        .map(&:lexeme)
+        .join
+    end
+
+    def call
+      return(Success(tokens)) unless str.match?(Re)
+
+      result = Emendate::SegmentSets::MixedSet.new
+      result << Emendate::DateTypes::Unprocessable.new(
+        children: tokens.segments
+      )
       result.warnings << 'Unprocessable string'
-      
+      Failure(result)
     end
 
     private
 
-    def passthrough
-      @result = Emendate::SegmentSets::MixedSet.new.copy(@tokens)
-    end
+    attr_reader :tokens, :str
   end
 end
