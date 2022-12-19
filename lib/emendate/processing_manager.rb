d9f9448116e6fc9f6db521c9cc53e1731d60383c
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-12-18 19:18:52 -0500

    Add new UntokenizableTagger to ProcessingManager

--- lib/emendate/processing_manager.rb
+++ lib/emendate/processing_manager.rb
@@ -9,7 +9,7 @@ module Emendate
 
     class << self
       def call(...)
-        self.initialize(...).call
+        self.new(...).call
       end
     end
 
@@ -25,37 +25,33 @@ module Emendate
     end
 
     def call
-      norm = yield Emendate.normalize(orig_string)
-      lexed = yield Emendate::Lexer.call(norm)
-      @tokens = lexed.tokens
-      @state = :lexed
-
-      untokenizeable = yield Emendate::UntokenizableTagger.call(
-        tokens: tokens,
-        str: orig_string
+      _lexed = yield handle_step(
+        state: :lexed,
+        proc: ->{ Emendate::Lexer.call(orig_string) }
+      )
+      _untokenizable_tagged = yield handle_step(
+        state: :untokenizable_tagged,
+        proc: ->{ Emendate::UntokenizableTagger.call(tokens) }
       )
 
       Success(self)
     end
 
-    def process
-      lex
-      tag_untokenizable if may_tag_untokenizable?
-      exit_if_untokenizable if may_exit_if_untokenizable?
-      tag_unprocessable if may_tag_unprocessable?
-      exit_if_unprocessable if may_exit_if_unprocessable?
-      tag_known_unknown if may_tag_known_unknown?
-      exit_if_known_unknown if may_exit_if_known_unknown?
-      collapse_tokens if may_collapse_tokens?
-      convert_months if may_convert_months?
-      translate_ordinals if may_translate_ordinals?
-      certainty_check if may_certainty_check?
-      standardize_formats if may_standardize_formats?
-      tag_date_parts if may_tag_date_parts?
-      segment_dates if may_segment_dates?
-      indicate_ranges if may_indicate_ranges?
-      finalize if may_finalize?
-    end
+    # def process
+    #   tag_unprocessable if may_tag_unprocessable?
+    #   exit_if_unprocessable if may_exit_if_unprocessable?
+    #   tag_known_unknown if may_tag_known_unknown?
+    #   exit_if_known_unknown if may_exit_if_known_unknown?
+    #   collapse_tokens if may_collapse_tokens?
+    #   convert_months if may_convert_months?
+    #   translate_ordinals if may_translate_ordinals?
+    #   certainty_check if may_certainty_check?
+    #   standardize_formats if may_standardize_formats?
+    #   tag_date_parts if may_tag_date_parts?
+    #   segment_dates if may_segment_dates?
+    #   indicate_ranges if may_indicate_ranges?
+    #   finalize if may_finalize?
+    # end
 
     def to_s
       <<~OBJ
@@ -72,154 +68,27 @@ module Emendate
 
     private
 
-    def perform_check_final_segments
-      return unless errors.empty?
-
-      tokens.each do |t|
-        next if t.date_type?
-        next if t.type == :or
-        next if t.type == :and
-
-        errors << 'Unhandled segment still present'
-      end
+    def handle_step(state:, proc:)
+      proc.call.either(
+        ->(success) do
+          @tokens = success
+          @state = state
+          add_warnings(success.warnings)
+          Success()
+        end,
+        ->(failure) do
+          errors << failure
+          @state = "#{state}_failure".to_sym
+          add_warnings(failure.warnings)
+          Failure(self)
+        end
+      )
     end
 
-    def perform_lex
-      l = Emendate::Lexer.new(norm_string)
-      l.tokenize
-      @norm_string = l.norm
-      @tokens = l.tokens
-      @orig_tokens = tokens.class.new.copy(tokens)
-    end
+    def add_warnings(new_warnings)
+      return if new_warnings.empty?
 
-    def perform_collapse_tokens
-      c = Emendate::TokenCollapser.new(tokens: tokens)
-      c.collapse
-      @tokens = c.result
-      @collapsed_tokens = tokens.class.new.copy(tokens)
-    end
-
-    def perform_convert_months
-      c = Emendate::AlphaMonthConverter.new(tokens: tokens)
-      c.convert
-      @tokens = c.result
-      @converted_months = tokens.class.new.copy(tokens)
-    end
-
-    def perform_translate_ordinals
-      t = Emendate::OrdinalTranslator.new(tokens: converted_months)
-      begin
-        t.translate
-      rescue StandardError => e
-        errors << e
-      else
-        @tokens = t.result
-        @translated_ordinals = tokens.class.new.copy(tokens)
-      end
-    end
-
-    def perform_certainty_check
-      c = Emendate::CertaintyChecker.new(tokens: translated_ordinals)
-      begin
-        c.check
-      rescue StandardError => e
-        errors << e
-      else
-        @tokens = c.result
-        @certainty_checked = tokens.class.new.copy(tokens)
-      end
-    end
-
-    def perform_standardize_formats
-      f = Emendate::FormatStandardizer.new(tokens: tokens)
-      begin
-        f.standardize
-      rescue StandardError => e
-        errors << e
-      else
-        @tokens = f.result
-        @standardized_formats = tokens.class.new.copy(tokens)
-      end
-    end
-
-    def perform_tag_date_parts
-      t = Emendate::DatePartTagger.new(tokens: standardized_formats)
-      begin
-        t.tag
-      rescue StandardError => e
-        errors << e
-      else
-        @tokens = t.result
-        @tagged_date_parts = tokens.class.new.copy(tokens)
-      end
-    end
-
-    def perform_tag_unprocessable
-      t = Emendate::UnprocessableTagger.new(tokens: tagged_untokenizable, str: orig_string)
-      begin
-        t.tag
-      rescue StandardError => e
-        errors << e
-      else
-        @tokens = t.result
-        @tagged_unprocessable = tokens.class.new.copy(tokens)
-      end
-    end
-
-    def perform_tag_untokenizable
-      t = Emendate::UntokenizableTagger.new(tokens: tokens, str: orig_string)
-      begin
-        t.tag
-      rescue StandardError => e
-        errors << e
-      else
-        @tokens = t.result
-        @tagged_untokenizable = tokens.class.new.copy(tokens)
-      end
-    end
-
-    def perform_tag_known_unknown
-      t = Emendate::KnownUnknownTagger.new(tokens: tokens, str: orig_string)
-      begin
-        t.tag
-      rescue StandardError => e
-        errors << e
-      else
-        @tokens = t.result
-        @tagged_known_unknown = tokens.class.new.copy(tokens)
-      end
-    end
-
-    def perform_segment_dates
-      s = Emendate::DateSegmenter.new(tokens: tagged_date_parts)
-      begin
-        s.segment
-      rescue StandardError => e
-        errors << e
-      else
-        @tokens = s.result
-        @segmented_dates = tokens.class.new.copy(tokens)
-      end
-    end
-
-    def perform_indicate_ranges
-      i = Emendate::RangeIndicator.new(tokens: segmented_dates)
-      begin
-        i.indicate
-      rescue StandardError => e
-        errors << e
-      else
-        @tokens = i.result
-        @ranges_indicated = tokens.class.new.copy(tokens)
-      end
-    end
-
-    def log_status_change
-      # puts "changing from #{aasm.from_state} to #{aasm.to_state} (event: #{aasm.current_event})"
-    end
-
-    def gather_warnings
-      warnings << tokens.warnings unless tokens.warnings.empty?
+      warnings << new_warnings
       warnings.flatten!
       warnings.uniq!
     end
@@ -234,10 +103,6 @@ module Emendate
 
     def unprocessable?
       tokens.types == [:unprocessable_date_type]
-    end
-
-    def untokenizable?
-      tokens.types == [:untokenizable_date_type]
     end
 
     def known_unknown?
