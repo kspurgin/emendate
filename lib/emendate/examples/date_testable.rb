a4853a2dec5d5efacced4227a6cc08a855900467
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-03-31 17:24:15 -0400

    Instance_eval expected result strings beginning with 'Date' in examples

--- lib/emendate/examples/date_testable.rb
+++ lib/emendate/examples/date_testable.rb
@@ -10,13 +10,14 @@ module Emendate
 
         example.rows
           .map{ |row| row.send(name.to_sym) }
+          .map{ |val| val.start_with?('Date') ? instance_eval(val) : val }
           .join('|')
       end
 
       def tested_result
         return nil unless example.testable?
         return 'nilValue' if example.processed.result.dates.empty?
-        
+
         example.processed.result.dates
           .map{ |date| date.send(name.to_sym) }
           .map{ |date| date.nil? ? 'nilValue' : date }
