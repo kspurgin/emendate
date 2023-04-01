59220ffe05009a237768582570f0f768eba27668
kspurgin <kspurgin@users.noreply.github.com>
2023-04-01 18:40:52 +0000

    deploy: aed7f7bcfdd96429c9c08bd0502bfe3e9119788c

--- lib/emendate/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Emendate
-  VERSION = '0.1.1'
-end
--- utils/document_example_set.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require 'date'
-
-# require_relative '../lib/emendate'
-require_relative '../spec/helpers'
-
-puts Helpers::META
