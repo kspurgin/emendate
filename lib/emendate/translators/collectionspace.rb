ff99f34a4f2ac664341e149c096889c020eb038c
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-04-03 17:00:47 -0400

    Add CS translation support for "before" range switch

--- lib/emendate/translators/collectionspace.rb
+++ lib/emendate/translators/collectionspace.rb
@@ -8,6 +8,7 @@ module Emendate
 
       def self.extended(mod)
         Emendate.config.options.bce_handling = :naive
+        Emendate.config.options.before_date_treatment = :range
       end
 
       def base_value
@@ -34,8 +35,19 @@ module Emendate
       end
 
       def computed
+        case date.range_switch
+        when 'after'
+          # not yet implemented
+        when 'before'
+          computed_before
+        else computed_normal
+        end
+      end
+
+      def computed_normal
         start_date = Date.parse(date.date_start_full)
         end_date = Date.parse(date.date_end_full)
+
         base_value.merge({
           scalarValuesComputed: 'true',
           dateEarliestScalarValue: "#{date.date_start_full}#{SUFFIX}",
@@ -51,15 +63,38 @@ module Emendate
         })
       end
 
+      def computed_before
+        end_date = Date.parse(date.date_end_full) + 1
+
+        base_value.merge({
+          scalarValuesComputed: 'true',
+          dateLatestScalarValue: "#{end_date}#{SUFFIX}",
+          dateLatestYear: end_date.year.to_s,
+          dateLatestMonth: end_date.month.to_s,
+          dateLatestDay: end_date.day.to_s,
+          dateLatestEra: 'CE',
+          dateLatestCertainty: 'Before'
+        })
+      end
+
       def approximate
         lexed = processed.history[:lexed]
         types = lexed.types
         if types.any?(:about)
-          computed.merge({dateEarliestSingleCertainty: 'about'})
+          computed.merge({
+              dateEarliestSingleCertainty: 'about',
+              dateLatestCertainty: 'about'
+            })
         elsif types.any?(:approximate)
-          computed.merge({dateEarliestSingleCertainty: 'Approximate'})
+          computed.merge({
+            dateEarliestSingleCertainty: 'Approximate',
+            dateLatestCertainty: 'Approximate'
+          })
         elsif types.any?(:circa)
-          computed.merge({dateEarliestSingleCertainty: 'Circa'})
+          computed.merge({
+            dateEarliestSingleCertainty: 'Circa',
+            dateLatestCertainty: 'Circa'
+          })
         end
       end
 
@@ -69,6 +104,13 @@ module Emendate
 
       def one_of_range_set
         raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+      end
+
+      def uncertain
+          computed.merge({
+            dateEarliestSingleCertainty: 'Possibly',
+            dateLatestCertainty: 'Possibly'
+          })
       end
     end
   end
