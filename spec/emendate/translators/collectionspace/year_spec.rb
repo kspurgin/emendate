07dac976c8ea9d36efb53dd069f7f08e0a9fc4fd
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-04-03 17:00:20 -0400

    Add CS translation uncertainty qualification

--- spec/emendate/translators/collectionspace/year_spec.rb
+++ spec/emendate/translators/collectionspace/year_spec.rb
@@ -36,6 +36,32 @@ RSpec.describe Emendate::Translators::Collectionspace::Year do
     end
   end
 
+  context 'with 2012?' do
+    let(:str){ '2012?' }
+    let(:expected) do
+      {
+        dateDisplayDate: '2012?',
+        scalarValuesComputed: 'true',
+        dateEarliestScalarValue: '2012-01-01T00:00:00.000Z',
+        dateEarliestSingleYear: '2012',
+        dateEarliestSingleMonth: '1',
+        dateEarliestSingleDay: '1',
+        dateEarliestSingleEra: 'CE',
+        dateEarliestSingleCertainty: 'Possibly',
+        dateLatestScalarValue: '2012-12-31T00:00:00.000Z',
+        dateLatestYear: '2012',
+        dateLatestMonth: '12',
+        dateLatestDay: '31',
+        dateLatestEra: 'CE',
+        dateLatestCertainty: 'Possibly',
+      }
+    end
+    it 'translates as expected' do
+      expect(value).to eq(expected)
+      expect(warnings).to be_empty
+    end
+  end
+
   context 'with 2002 B.C.' do
     let(:str){ '2002 B.C.' }
     let(:expected) do
