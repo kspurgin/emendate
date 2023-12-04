437c8aed276a5a10d2049740199c57fa3277d14b
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-12-04 12:28:10 -0500

    Create new lexer based on StringScanner

---
+++ lib/emendate/scan_lexer.rb
@@ -0,0 +1,244 @@
+# frozen_string_literal: true
+
+require 'strscan'
+
+require 'emendate/date_utils'
+require 'emendate/location'
+
+module Emendate
+  class ScanLexer
+    include Dry::Monads[:result]
+    include Dry::Monads::Do.for(:call)
+    include DateUtils
+
+    class << self
+      def call(...)
+        new(...).call
+      end
+    end
+
+    SINGLES = {
+      "'" => :apostrophe,
+      ':' => :colon,
+      ',' => :comma,
+      '{' => :curly_bracket_open,
+      '}' => :curly_bracket_close,
+      '%' => :percent,
+      '+' => :plus,
+      '?' => :question,
+      '/' => :slash,
+      ' ' => :space,
+      '[' => :square_bracket_open,
+      ']' => :square_bracket_close,
+      '~' => :tilde
+    }
+    ["\u002D", "\u2010", "\u2011", "\u2012", "\u2013", "\u2014", "\u2015",
+     "\u2043"].each{ |val| SINGLES[val] = :hyphen }
+    SINGLES.freeze
+
+    ORDINAL_INDICATORS = %w[st nd rd th d].freeze
+
+    days = '^(' + ([
+      Date::DAYNAMES.compact,
+      Date::ABBR_DAYNAMES.compact.map{ |val| val + '\.?' }
+    ].flatten
+      .join('|') + ')')
+    months = '^(' + ([
+      Date::MONTHNAMES.compact,
+      Date::ABBR_MONTHNAMES.compact.map{ |val| val + '\.?' }
+    ].flatten
+      .join('|') + ')')
+
+    ALPHA = {
+      /^(about|around)/i => :about,
+      /^(after|post)/i => :after,
+      /^(&|and)/i => :and,
+      /^(approximate(ly|)|estimated?|est\.?)/i => :approximate,
+      /^present/i => :present,
+      /^(before|pre|prior to)/i => :before,
+      /^(century|cent\.?)/i => :century,
+      /^(ca\.?|circa)/i => :circa,
+      Regexp.new(days, 'i') => :day_of_week_alpha,
+      /^(b\.? ?c\.? ?e\.?|c\.? ?e\.?|b\.? ?p\.?)/i => :era,
+      /^(early|late|middle|mid)/i => :partial,
+      Regexp.new(months, 'i') => :month_alpha,
+      /^or/i => :or,
+      /^to/i => :range_indicator,
+      # If additional seasons are added, make sure to update the mapping to
+      #   literals in AlphaMonthConverter
+      /^(winter|spring|summer|fall|autumn)/i => :season,
+      /^(date unknown|no date|not dated|unknown( date|)|unk|n\.? ?d\.?)/i =>
+        :unknown_date,
+      /^(u+|x+)/i => :uncertainty_digits
+    }
+
+    SINGLE_ALPHA = /^[cestyz]$/i
+
+    def initialize(str)
+      @tokens = Emendate::SegmentSets::TokenSet.new(string: str)
+      @scanner = StringScanner.new(str)
+    end
+
+    def call
+      _tokenized = yield tokenize
+
+      Success(tokens)
+    end
+
+    private
+
+    attr_reader :tokens, :scanner
+
+    def tokenize
+      tokenize_anchored_start
+      tokenization until scanner.eos?
+    rescue StandardError => e
+      Failure(e)
+    else
+      Success()
+    end
+
+    def tokenization
+      nextchar = scanner.getch
+      scanner.unscan
+
+      token = if SINGLES.key?(nextchar)
+                tokenize_single
+              elsif nextchar == '.'
+                tokenize_dots
+              elsif digit?(nextchar)
+                tokenize_number
+              elsif alpha?(nextchar)
+                tokenize_letter(nextchar)
+              end
+      return if token
+
+      init = scanner.pos
+      match = scanner.getch
+      add_token(match, :unknown, init)
+    end
+
+    def tokenize_anchored_start
+      case scanner.string
+      when /^c\.? ?[^a-z]/i then tokenize_starting_circa
+      end
+    end
+
+    def tokenize_starting_circa
+      init = scanner.pos
+      match = scanner.scan(/^c\.? ?/i)
+      add_token(match, :circa, init)
+    end
+
+    def tokenize_single
+      init = scanner.pos
+      match = scanner.getch
+      add_token(match, SINGLES[match], init)
+      true
+    end
+
+    def tokenize_dots
+      init = scanner.pos
+      match = scanner.scan(/\.+/)
+      type = case match.length
+             when 1
+               :single_dot
+             when 2
+               :double_dot
+             else
+               :unknown
+             end
+      add_token(match, type, init)
+      true
+    end
+
+    def tokenize_number
+      init = scanner.pos
+      match = scanner.scan(/\d+/)
+      tokens << NumberToken.new(
+        type: :number, lexeme: match, location: location(init)
+      )
+      tokenize_ordinal_indicator
+    end
+
+    def tokenize_ordinal_indicator
+      indicator = ORDINAL_INDICATORS.find{ |ind| ordinal_val_match?(ind) }
+      return true unless indicator
+
+      init = scanner.pos
+      pattern = Regexp.new(indicator, Regexp::IGNORECASE)
+      match = scanner.scan(pattern)
+      add_token(match, :ordinal_indicator, init)
+      true
+    end
+
+    def ordinal_val_match?(str)
+      chk = scanner.peek(str.length).downcase
+      str.downcase == chk
+    end
+
+    def tokenize_letter(char)
+      pattern = alpha_matcher
+
+      if pattern
+        tokenize_alpha_pattern(pattern)
+      elsif SINGLE_ALPHA.match?(char)
+        tokenize_single_alpha
+      else
+        tokenize_unknown_alpha
+      end
+      true
+    end
+
+    def alpha_matcher
+      chk = scanner.rest
+      ALPHA.keys.find{ |regexp| regexp.match?(chk) }
+    end
+
+    def tokenize_alpha_pattern(pattern)
+      init = scanner.pos
+      match = scanner.scan(pattern)
+      if alpha?(scanner.peek(1))
+        addtl = scanner.scan(/[a-z]+/i)
+        add_token(match + addtl, :unknown, init)
+      else
+        add_token(match, ALPHA[pattern], init)
+      end
+    end
+
+    def tokenize_single_alpha
+      init = scanner.pos
+      char = scanner.scan(/./)
+      type = "letter_#{char}".to_sym
+      add_token(char, type, init)
+    end
+
+    def tokenize_unknown_alpha
+      init = scanner.pos
+      match = scanner.scan(/[a-z]+/i)
+      add_token(match, :unknown, init)
+    end
+
+    # @param lexeme [String]
+    # @param type [Symbol]
+    # @param init [Integer]
+    def add_token(lexeme, type, init)
+      tokens << Token.new(
+        lexeme: lexeme, type: type, location: location(init)
+      )
+    end
+
+    def location(startpos)
+      length = scanner.pos - startpos
+      Emendate::Location.new(startpos, length)
+    end
+
+    def digit?(char)
+      char >= '0' && char <= '9'
+    end
+
+    def alpha?(char)
+      /[a-z&]/i.match?(char)
+    end
+  end
+end
---
+++ spec/emendate/scan_lexer_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Emendate::ScanLexer do
+  subject(:lexer){ described_class }
+
+  describe '.call' do
+    it 'returns expected tokens' do
+      examples = {
+        'c.' => [:circa],
+        'c 1947' => %i[circa number4],
+        '2nd' => %i[number1or2 ordinal_indicator],
+        'c1947' => %i[circa number4],
+        '1919 andor 1950' => %i[number4 space unknown space number4],
+        '@' => [:unknown],
+        'Sep. 1' => %i[month_alpha space number1or2],
+        'cat' => [:unknown],
+        'Sep. 19, 1918' => %i[month_alpha space number1or2 comma space
+                              number4],
+        '{..1984' => %i[curly_bracket_open double_dot number4],
+        '{...1984' => %i[curly_bracket_open unknown number4],
+        '- –' => %i[hyphen space hyphen],
+        '?' => [:question],
+        '/' => [:slash],
+        '[]' => %i[square_bracket_open square_bracket_close],
+        '1 22 333 4444' => %i[number1or2 space number1or2 space number3 space
+                              number4],
+        '4444.0' => %i[number4 single_dot standalone_zero],
+        'August Jan' => %i[month_alpha space month_alpha],
+        'Mon Tuesday' => %i[day_of_week_alpha space day_of_week_alpha],
+        'x xx uuu' => %i[uncertainty_digits space uncertainty_digits space
+                         uncertainty_digits],
+        # NOTE: c isn't first in string or it'd get normalized to circa
+        'e c s t y z' => %i[letter_e space letter_c space letter_s space letter_t
+                            space letter_y space letter_z],
+        'cent century' => %i[century space century],
+        'about around' => %i[about space about],
+        'approximately estimated' => %i[approximate space approximate],
+        'c ca circa' => %i[circa circa space circa],
+        'unknown' => [:unknown_date],
+        'n.d.' => [:unknown_date],
+        'n. d.' => [:unknown_date],
+        'or' => [:or],
+        'b.c.e bp c.e.' => %i[era space era space era],
+        'early late middle mid' => %i[partial space partial space partial space
+                                      partial],
+        'before pre after post' => %i[before space before space after space
+                                      after],
+        '& and' => %i[and space and],
+        'Spring Winter Fall' => %i[season space season space season],
+        '+%~{}:' => %i[plus percent tilde curly_bracket_open curly_bracket_close
+                       colon],
+        'to' => [:range_indicator],
+        '1974-present' => %i[number4 hyphen present]
+      }
+
+      results = examples.keys
+                        .map do |str|
+        [
+          str,
+          lexer.call(str)
+               .value!
+               .map(&:type)
+        ]
+      end
+                        .to_h
+      expect(results).to eq examples
+    end
+  end
+end
