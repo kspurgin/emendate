38e915290fe98c0ae1d082306a4a7aa2721bff3e
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2024-08-14 13:01:06 -0400

    Strip punctuation from Qualifier lexeme (convert Qualifier to class)
    This previously was a Data subclass

--- lib/emendate/qualifier.rb
+++ lib/emendate/qualifier.rb
@@ -1,15 +1,31 @@
 # frozen_string_literal: true
 
 module Emendate
-  # @param type [:approximate, :approximate_and_uncertain, :inferred,
-  #   :uncertain]
-  # @param precision [:whole, :leftward, :rightward, :single_segment, :unknown]
-  # @param lexeme [String]
-  # @param sources [Array<Emendate::Segment>]
-  Qualifier = Data.define(:type, :precision, :lexeme) do
-    def initialize(type:, precision:, lexeme: "")
-      super
+  class Qualifier
+    include Comparable
+
+    attr_reader :type, :precision, :lexeme
+
+    # @param type [:approximate, :approximate_and_uncertain, :inferred,
+    #   :uncertain]
+    # @param precision [:whole, :leftward, :rightward, :single_segment,
+    #   :unknown]
+    # @param lexeme [String, nil]
+    def initialize(type:, precision:, lexeme: nil)
+      @type = type
+      @precision = precision
+      @lexeme = clean_lexeme(lexeme)
     end
+
+    def <=>(other)
+      return unless other.is_a?(self.class)
+
+      signature <=> other.signature
+    end
+
+    def hash = signature.hash
+
+    def eql?(other) = self.class == other.class && self == other
 
     def to_s
       "#<#{self.class.name} type=#{type.inspect}, "\
@@ -20,6 +36,16 @@ module Emendate
 
     def for_test
       "#{type}, #{precision}"
+    end
+
+    def signature = [type, precision, lexeme].join("|")
+
+    private
+
+    def clean_lexeme(str)
+      return "" unless str
+
+      str.gsub(/\p{Punct}/, "")
     end
   end
 end
