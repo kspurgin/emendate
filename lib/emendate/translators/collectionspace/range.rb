e5a064e5739d81ee69b6344f033f0c035c27ac94
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2024-08-14 13:04:50 -0400

    Override base qualify method

--- lib/emendate/translators/collectionspace/range.rb
+++ lib/emendate/translators/collectionspace/range.rb
@@ -14,6 +14,43 @@ module Emendate
           @base = computed
           qualify
         end
+
+        private
+
+        def qualify_qualifiers
+          return if pdate.certain?
+
+          vals = qual_hash(pdate)
+          vals.reject! { |key, val| val.empty? }
+
+          if vals.key?(:start)
+            @qualified = @qualified.merge({
+              dateEarliestSingleCertainty: vals[:start]
+            })
+          end
+          if vals.key?(:end)
+            @qualified = @qualified.merge({dateLatestCertainty: vals[:end]})
+          end
+        end
+
+        def qual_hash(pdate)
+          h = {
+            start: [],
+            end: []
+          }
+          pdate.qualifiers.dup.each do |qual|
+            precision = qual.precision
+            if precision == :whole
+              term = qualifier_term(qual)
+              %i[start end].each { |prec| h[prec] << term }
+            else
+              h[qual.precision] << qualifier_term(qual)
+            end
+          end
+          h.transform_values do |arr|
+            arr.compact.uniq.sort.join(", ").capitalize
+          end
+        end
       end
     end
   end
