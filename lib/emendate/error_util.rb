66a45f74c6e62c020ae668a04b692df21a3e7476
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2024-08-16 21:27:06 -0400

    Handle DateType::Error

--- lib/emendate/error_util.rb
+++ lib/emendate/error_util.rb
@@ -6,7 +6,11 @@ module Emendate
     module_function
 
     def msg(err)
-      [err.message, err.backtrace.first(5)].flatten
+      if err.is_a?(Exception)
+        [err.message, err.backtrace.first(5)].flatten
+      elsif err.is_a?(Emendate::DateTypes::Error)
+        [err.error_type.to_s.capitalize, err.message]
+      end
     end
   end
 end
